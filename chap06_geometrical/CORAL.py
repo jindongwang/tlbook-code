6328b2bf0438ab3032590b996cdca5a43d144436
Jindong Wang <jindongwang@outlook.com>
2022-02-22 18:12:02 +0900

    format

--- chap06_geometrical/CORAL.py
+++ chap06_geometrical/CORAL.py
@@ -24,8 +24,9 @@ class CORAL:
         '''
         cov_src = np.cov(Xs.T) + np.eye(Xs.shape[1])
         cov_tar = np.cov(Xt.T) + np.eye(Xt.shape[1])
-        A_coral = np.dot(scipy.linalg.fractional_matrix_power(cov_src, -0.5),
-                         scipy.linalg.fractional_matrix_power(cov_tar, 0.5))
+        A_coral = np.dot(
+            scipy.linalg.fractional_matrix_power(cov_src, -0.5),
+            scipy.linalg.fractional_matrix_power(cov_tar, 0.5))
         Xs_new = np.real(np.dot(Xs, A_coral))
         return Xs_new
 
@@ -52,7 +53,8 @@ if __name__ == '__main__':
         for j in range(4):
             if i != j:
                 src, tar = 'data/' + domains[i], 'data/' + domains[j]
-                src_domain, tar_domain = scipy.io.loadmat(src), scipy.io.loadmat(tar)
+                src_domain, tar_domain = scipy.io.loadmat(
+                    src), scipy.io.loadmat(tar)
                 Xs, Ys, Xt, Yt = src_domain['feas'], src_domain['label'], tar_domain['feas'], tar_domain['label']
                 coral = CORAL()
                 acc, ypre = coral.fit_predict(Xs, Ys, Xt, Yt)
